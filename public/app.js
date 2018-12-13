8deaaaca9e2edd6c6578298fc3acbbeb950985c5
ChanR <chan.rahar@gmail.om>
2018-12-12 22:06:16 -0500

    updated functionality

--- public/app.js
+++ public/app.js
@@ -4,78 +4,37 @@ $(document).ready(function () {
   });
 
   // save articles to the db
-  $('.save-button').on("click", function () { 
+  $('.save-button').on("click", function () {
     console.log("clicked")
+    alert("Article Saved");
     $.post("/articles/", {
-      title:$(this).attr("title"),
+      title: $(this).attr("title"),
       link: $(this).attr("link")
-    }, function() {
+    }, function () {
       window.location.href = "#";
     });
   });
 
-  
-
-  // Whenever someone clicks a p tag
-  $(document).on("click", "p", function () {
-    // Empty the notes from the note section
-    $("#notes").empty();
-    // Save the id from the p tag
+  $(".save-comment").on("click", function () {
     var thisId = $(this).attr("data-id");
+    console.log(thisId);
+    var data = {
+      body: $("#text" + thisId).val()
+    }
+    console.log(data);
+    $.post("/articles/" + thisId, data, function () {
+      window.location.href = "#";
+    });
+  })
 
-    // Now make an ajax call for the Article
-    $.ajax({
-      method: "GET",
-      url: "/articles/" + thisId
-    })
-      // With that done, add the note information to the page
-      .then(function (data) {
-        console.log(data);
-        // The title of the article
-        $("#notes").append("<h2>" + data.title + "</h2>");
-        // An input to enter a new title
-        $("#notes").append("<input id='titleinput' name='title' >");
-        // A textarea to add a new note body
-        $("#notes").append("<textarea id='bodyinput' name='body'></textarea>");
-        // A button to submit a new note, with the id of the article saved to it
-        $("#notes").append("<button data-id='" + data._id + "' id='savenote'>Save Note</button>");
-
-        // If there's a note in the article
-        if (data.note) {
-          // Place the title of the note in the title input
-          $("#titleinput").val(data.note.title);
-          // Place the body of the note in the body textarea
-          $("#bodyinput").val(data.note.body);
-        }
-      });
-  });
-
-  // When you click the savenote button
-  $(document).on("click", "#savenote", function () {
-    // Grab the id associated with the article from the submit button
+  $(".comment-btn").on("click", function () {
     var thisId = $(this).attr("data-id");
+    console.log(thisId)
+    $.get("/articles/" + thisId, function (data) {
+      console.log(data);
+      $("#" + thisId).val(data.note.body)
+    });
+  })
 
-    // Run a POST request to change the note, using what's entered in the inputs
-    $.ajax({
-      method: "POST",
-      url: "/articles/" + thisId,
-      data: {
-        // Value taken from title input
-        title: $("#titleinput").val(),
-        // Value taken from note textarea
-        body: $("#bodyinput").val()
-      }
-    })
-      // With that done
-      .then(function (data) {
-        // Log the response
-        console.log(data);
-        // Empty the notes section
-        $("#notes").empty();
-      });
 
-    // Also, remove the values entered in the input and textarea for note entry
-    $("#titleinput").val("");
-    $("#bodyinput").val("");
-  });
 });
--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,8 +1,43 @@
 var db = require("../models");
 
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
+var axios = require("axios");
+var cheerio = require("cheerio");
+
 module.exports = function (app) {
 
 app.get("/", (req, res) => res.render("index"));
+
+app.get("/scrape", function (req, res) {
+  // First, we grab the body of the html with axios
+  axios.get("https://www.reddit.com/r/news/").then(function (response) {
+    // Then, we load that into cheerio and save it to $ for a shorthand selector
+    // Save an empty result object
+    var $ = cheerio.load(response.data);
+    var handlebarsObject = {
+      data: []
+    };
+
+
+    $("a.b5szba-0").each(function (i, element) {
+
+      if ($(element).parent().parent().children().children().children().children("h2").text() === "") {
+        console.log("ad")
+      } else {
+        // Add the text and href of every link, and save them as properties of the result object
+        handlebarsObject.data.push({
+          title: $(element).parent().parent().children().children().children().children("h2").text(),
+          link: $(element).attr("href")
+        });
+      }
+
+    });
+    // If we were able to successfully scrape and save an Article, send a message to the client
+    res.render("index", handlebarsObject);
+  });
+});
+
 
 // app.get("/saved", (req, res) => res.render("saved", {
 //     article: dbArticle
